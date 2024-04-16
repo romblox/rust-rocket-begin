264d51b15906f645b5c39b261d9c88fe461d8bbf
Ruslan Romanenko <romanenko_rn@groupbwt.com>
2024-04-16 19:52:10 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+mod auth;
+
+#[macro_use] extern crate rocket;
+
+use rocket::serde::json::{Value, json};
+use rocket::response::status;
+use auth::BasicAuth;
+
+
+#[get("/rustaceans")]
+fn get_rustaceans(_auth: BasicAuth) -> Value {
+    json!([{ "id": 1, "name": "John Doe" }, { "id": 2, "name": "John Doe again" }])
+}
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32, _auth: BasicAuth) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+#[post("/rustaceans", format = "json")]
+fn create_rustacean(_auth: BasicAuth) -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+}
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32, _auth: BasicAuth) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32, _auth: BasicAuth) -> status::NoContent {
+    status::NoContent
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!("Not found")
+}
+
+#[catch(401)]
+fn unauthorized() -> Value {
+    json!("Unauthorized")
+}
+
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![
+            get_rustaceans,
+            view_rustacean,
+            create_rustacean,
+            update_rustacean,
+            delete_rustacean,
+
+        ])
+        .register("/", catchers![
+            not_found,
+            unauthorized,
+        ])
+        .launch()
+        .await;
+}

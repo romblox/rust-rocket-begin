3e14f6e88b648bd688e880c468ec327960a85d4e
Ruslan Romanenko <romanenko_rn@groupbwt.com>
2024-04-17 12:22:28 +0300

    query inserting new rustacean

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ mod models;
 #[macro_use] extern crate rocket;
 
 use diesel::prelude::*;
-use rocket::serde::json::{Value, json};
+use rocket::serde::json::{Value, json, Json};
 use rocket::response::status;
 use rocket_sync_db_pools::database;
 use auth::BasicAuth;
 use schema::rustaceans;
-use models::Rustacean;
+use models::{Rustacean, NewRustacean};
+
 
 #[database("sqlite")]
 struct DbConn(diesel::SqliteConnection);
@@ -22,7 +23,7 @@ async fn get_rustaceans(_auth: BasicAuth, db: DbConn) -> Value {
             .order(rustaceans::id.desc())
             .limit(1000)
             .load::<Rustacean>(c)
-            .expect("DB error");
+            .expect("DB error while get all rustaceans");
         json!(result)
     }).await
 }
@@ -30,9 +31,14 @@ async fn get_rustaceans(_auth: BasicAuth, db: DbConn) -> Value {
 fn view_rustacean(id: i32, _auth: BasicAuth) -> Value {
     json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
 }
-#[post("/rustaceans", format = "json")]
-fn create_rustacean(_auth: BasicAuth) -> Value {
-    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+#[post("/rustaceans", format = "json", data = "<new_rustacean>")]
+async fn create_rustacean(_auth: BasicAuth, db: DbConn, new_rustacean: Json<NewRustacean>) -> Value {
+    db.run(|c| {
+        let result = diesel::insert_into(rustaceans::table)
+            .values(new_rustacean.into_inner())
+            .execute(c).expect("DB error while inserting new rustacean");
+        json!(result)
+    }).await
 }
 #[put("/rustaceans/<id>", format = "json")]
 fn update_rustacean(id: i32, _auth: BasicAuth) -> Value {
@@ -53,6 +59,11 @@ fn unauthorized() -> Value {
     json!("Unauthorized")
 }
 
+#[catch(422)]
+fn unprocessable() -> Value {
+    json!("Invalid entity. Probably some missing fields?")
+}
+
 
 #[rocket::main]
 async fn main() {
@@ -68,6 +79,7 @@ async fn main() {
         .register("/", catchers![
             not_found,
             unauthorized,
+            unprocessable,
         ])
         .attach(DbConn::fairing())
         .launch()
